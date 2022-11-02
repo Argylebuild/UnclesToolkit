8ad0af8cfb41697bdb11a188aab13876a31f8ba1
Logan Smith <lmaxsmith@gmail.com>
2022-11-01 22:03:10 -0700

    Moved UnitConvert to toolbox

---
+++ UnitConvert.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Argyle.UnclesToolkit
+{
+	public static class UnitConvert
+	{
+		public static float MetersToFeet(this float meters)
+		{
+			return meters * 3.28084f;
+		}
+
+		public static float FeetToMeters(this float feet)
+		{
+			return feet / 3.28084f;
+		}
+
+    
+    
+		public static string MetersToFeetInchesString(this float meters)
+		{
+			float feetDecimal = meters.MetersToFeet();
+			int feetInt = Mathf.FloorToInt(feetDecimal);
+			decimal inchesRemainder =(decimal) (feetDecimal - feetInt) * 12;
+
+			return $"{feetInt}'-{decimal.Round(inchesRemainder, 2)}\"";
+		}
+	}
+}
