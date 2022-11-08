3b4d2b6b9a247fc67f3cf2be1a668998fd7a2a82
Logan Smith <lmaxsmith@gmail.com>
2022-11-07 18:16:26 -0800

    Added lifetimer

---
+++ LifeTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace Argyle.UnclesToolkit
+{
+	public class LifeTimer : ArgyleComponent
+	{
+		public float lifespan = 1;
+		public bool countOnStart = true;
+		
+		private void Start()
+		{
+			if(countOnStart)
+				Count();
+		}
+
+		public async void Count()
+		{
+			await UniTask.Delay((int) lifespan * 1000);
+			Destroy(GO);
+		}
+	}
+}
