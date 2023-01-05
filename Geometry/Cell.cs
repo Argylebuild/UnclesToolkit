bb0726fd9f3236597795dd9b3f1dffc74642de5c
Logan Smith <lmaxsmith@gmail.com>
2023-01-04 17:14:30 -0800

    First draft of lattice. Compiling but untested.

---
+++ Geometry/Cell.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Argyle.UnclesToolkit.Geometry
+{
+	public class Cell<T> where T : class
+	{
+		public HashSet<T> Things { get; } = new HashSet<T>();
+
+		public Lattice<T> LatticeParent { get; private set; }
+		
+		public Vector3Int CellPosition { get; private set; }
+		
+		#region ==== Settings ====------------------
+
+		public float CellSize => LatticeParent.CellSize;
+
+		#endregion -----------------/Settings ====
+
+	
+	
+	
+		#region ==== CTOR ====------------------
+
+		public Cell(Lattice<T> latticeParent, Vector3Int cellPosition)
+		{
+			LatticeParent = latticeParent;
+			CellPosition = cellPosition;
+		}
+
+		#endregion -----------------/CTOR ====
+
+
+
+		#region ==== CRUD ====------------------
+
+		public void Add(T thing)
+		{
+			Things.Add(thing);
+		}
+
+		#endregion -----------------/CRUD ====
+		
+	}
+}
