110c3366a7711e271aab121b271a89987a39efee
Logan Smith <lmaxsmith@gmail.com>
2023-04-10 17:03:09 -0700

    Option to keep decimal feet

--- UnitConvert.cs
+++ UnitConvert.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Argyle.UnclesToolkit
 {
@@ -16,17 +17,29 @@ namespace Argyle.UnclesToolkit
 
     
     
-		public static string MetersToFeetInchesString(this float meters, int dimensions = 1)
+		public static string MetersToFeetInchesString(this float meters, int dimensions = 1, bool useDecimalFeet = false)
 		{
 			float feetDecimal = meters.MetersToFeet(dimensions);
 			int feetInt = Mathf.FloorToInt(feetDecimal);
 			float inchesRemainder =(feetDecimal - feetInt) *  Mathf.Pow(12, dimensions);
 
-			if(dimensions == 1)
-				return $"{feetInt}'-{decimal.Round((decimal) inchesRemainder, 2)}\"";
+			if (useDecimalFeet)
+			{
+				if(dimensions == 1)
+					return $"{Math.Round(feetDecimal, 2)}'";
 
-			return $"{feetInt} ft<sup>{dimensions}</sup> - \n" +
-			       $"{decimal.Round((decimal) inchesRemainder, 2)} in<sup>{dimensions}</sup>)";
+				return $"{Math.Round(feetDecimal, 2)} ft<sup>{dimensions}</sup>";
+			}
+			else
+			{
+				if(dimensions == 1)
+					return $"{feetInt}'-{Math.Round(inchesRemainder, 2)}\"";
+
+				return $"{feetInt} ft<sup>{dimensions}</sup> - \n" +
+				       $"{Math.Round(inchesRemainder, 2)} in<sup>{dimensions}</sup>)";
+			}
+				
+			
 		}
 	}
 }
