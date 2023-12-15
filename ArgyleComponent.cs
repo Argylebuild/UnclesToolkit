ef8d4f0b0792fa36a698562f8cdbeb00ce96bea7
Logan Smith <lmaxsmith@gmail.com>
2023-12-15 03:09:17 -1000

    Fixed usage notes dropdown

--- ArgyleComponent.cs
+++ ArgyleComponent.cs
@@ -7,6 +7,7 @@ namespace Argyle.UnclesToolkit
 {
     public class ArgyleComponent : MonoBehaviour
     {
+        [TextArea]
         public string _usageNotes;
         
         private bool _isReferenceSet = false;
